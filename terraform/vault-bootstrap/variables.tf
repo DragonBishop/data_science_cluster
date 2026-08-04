74909584405bdf4e206ab24b31da372ec9fbbff8
DragonBishop <276789280+DragonBishop@users.noreply.github.com>
2026-08-04 03:56:13 +0000

    feat: add Terraform vault-bootstrap module (auth, kv, encryption)

---
+++ terraform/vault-bootstrap/variables.tf
@@ -0,0 +1,14 @@
+variable "postgres_superuser_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "s3_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "s3_secret_key" {
+  type      = string
+  sensitive = true
+}
